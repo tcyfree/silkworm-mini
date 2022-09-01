caac724e97f3e03687c57587d3adac98c03313a9
tangcy <program101@163.com>
2022-09-01 20:18:56 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+//app.js
+App({
+  onLaunch: function () {
+    if (!wx.cloud) {
+      console.error('请使用 2.2.3 或以上的基础库以使用云能力')
+    } else {
+      wx.cloud.init({
+        traceUser: true,
+        // 需要替换成自己的云开发环境ID
+        env: "wxa5a5845d3a5fb3ea"
+      })
+    }
+
+    this.globalData = {}
+  }
+})
